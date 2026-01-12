001e298908f3415c22902d2bc815c1b45b4ba303
Karthicksoundararajan1986 <Karthicksoundararajan01@gmail.com>
2026-01-12 20:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from flask import Flask, render_template  # 1. ADDED RENDER_TEMPLATE HERE
+
+app = Flask(__name__)
+
 @app.route('/')
 def home():
-    # If this line still says return "My Python website is running", 
-    # it will always stay blank. It MUST use render_template.
+    # 2. THIS TELLS FLASK TO LOOK FOR index.html IN THE templates FOLDER
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
